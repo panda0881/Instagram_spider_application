4d1b513a1acbfa7bef7aebb02eed80ca6b9f6acd
Hongming <hzhangal0330@gmail.com>
2016-08-26 23:24:27 +0800

    solve a bug in the spider

--- Instagram_Spider.py
+++ Instagram_Spider.py
@@ -94,9 +94,10 @@ class InstagramSpider:
         resp = requests.get(url)
         try:
             media = json.loads(resp.text)
-            self.numPosts += len(media['items'])
         except:
             print('there is something wrong with this user')
+            return
+        self.numPosts += len(media['items'])
         print('collecting data from ' + str(self.numPosts) + 'medias')
         for item in media['items']:
             self.full_media_list.append(item['code'])
